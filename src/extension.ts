1562d5535e62dd345bf7319ed5e71fdea4ccaa98
JR <jr@jr.com>
2023-10-02 22:04:49 -0300

    extensions

--- src/extension.ts
+++ src/extension.ts
@@ -28,23 +28,6 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.commands.registerCommand("make.relation.manager", async () => {
     relationManagersMake();
   });
-
-  /*
-	Snippets
-	protected static ?int $sort = 2;
-	protected static ?string $navigationIcon = 'heroicon-o-document-text';
-	protected static ?string $activeNavigationIcon = 'heroicon-o-document-text';
-	protected static ?string $navigationGroup = 'Settings';
-	protected static bool $shouldRegisterNavigation = false;
-
-	protected static ?string $title = 'Custom Page Title';
-	protected static ?string $navigationLabel = 'Custom Navigation Label';
-	protected static ?string $slug = 'custom-url-slug';
-	protected ?string $heading = 'Custom Page Heading';
-	protected ?string $subheading = 'Custom Page Subheading';
-	*/
-
-  //   context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
